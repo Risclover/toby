675df066d4363a776bbadb90d86f4a35cf3ffb4b
Sara Dunlop <sara091592@gmail.com>
2025-09-12 18:57:49 -0700

    properly configure for testing

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,18 @@
+import "../assets/styles/Dashboard.css"
+
 export const Dashboard = () => {
-    return <div></div>
+    return <div className="dashboard">
+        <div className="dashboard-titlebar">
+            <h1>Household Name</h1>
+            <div className="dashboard-titlebar-right">
+                <input type="search" name="search" id="search" placeholder="Search" />
+                <button>Bell</button>
+                <button>Gear</button>
+            </div>
+        </div>
+        <div className="dashboard-body">
+
+        </div>
+
+    </div>
 }
