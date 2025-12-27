998a4e4034893632455b29fa4fd6144ce9aa7c21
Sara Dunlop <sara091592@gmail.com>
2025-12-27 11:19:30 -0800

    ui(mobile-header): implement logic for household info

--- frontend/src/component/MobileHomeFamilyTitle.tsx
+++ frontend/src/component/MobileHomeFamilyTitle.tsx
@@ -1,21 +1,22 @@
+import { useAuthenticateQuery } from "@/store/authSlice"
+import { useGetHouseholdQuery } from "@/store/householdSlice";
 import { Avatar, Tooltip } from "@mantine/core"
+import { Link } from "react-router-dom";
 
 export const MobileHomeFamilyTitle = () => {
+    const { data: user } = useAuthenticateQuery();
+    const { data: household } = useGetHouseholdQuery(user?.householdId);
+
+    const additionalMembers = household?.members.length - 3;
     return (
         <div className="mobile-home-family-title">
-            The Sara Family
+            {household?.name}
 
             <Avatar.Group spacing="xs">
-                <Tooltip label="Sara" withArrow>
-                    <Avatar src="https://i.pravatar.cc/150?img=1" alt="Sara" radius="xl" size={26} />
-                </Tooltip>
-                <Tooltip label="John" withArrow>
-                    <Avatar src="https://i.pravatar.cc/150?img=2" alt="John" radius="xl" size={26} />
-                </Tooltip>
-                <Tooltip label="Emma" withArrow>
-                    <Avatar src="https://i.pravatar.cc/150?img=3" alt="Emma" radius="xl" size={26} />
-                </Tooltip>
-                <Avatar size={26}>+3</Avatar>
+                {household?.members.map(member => <Tooltip label={member.name} withArrow>
+                    <Avatar component={Link} to={`/users/${member.id}`} target="_blank" src={member.profileImg} alt={member.name} radius="xl" size={26} />
+                </Tooltip>).slice(0, 3)}
+                {additionalMembers > 0 && <Avatar size={26}>+{additionalMembers}</Avatar>}
             </Avatar.Group>
         </div>
     )
